80f733d543851a5db59a20760076da7faa9788cf
cl4pper <cl4pper@gmail.com>
2019-12-26 14:53:31 -0300

    ADD: create methods to insert and query data from db

--- server.js
+++ server.js
@@ -3,11 +3,47 @@ const express = require('express')
 const server = express()
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost/library', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to mongodb...'))
     .catch(err => console.error('NOT connect to mongobd!', err))
 
 const PORT_MAIN = 8080
+
+const bookSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+})
+
+const Book = mongoose.model('Book', bookSchema)
+
+// METHOD TO REGISTER DATA INTO DATABASE
+async function createBook () {
+    const book = new Book ({
+        title: 'Cronicas de Sharpe',
+        author: 'Bernard Cornwell',
+        tags: [ 'adventure', 'history' ],
+        isPublished: true
+    })
+
+    const result = await book.save()
+    console.log(result)
+}
+
+// METHOD TO GET DATA FROM DATABASE
+async function getBooks () {
+    const result = await Book.find()
+        .limit(1)
+        .sort({ title: 1 })
+        .select({ title: 1, tags: 1 })
+    console.log(result)
+}
+
+// CALLING MTHODS TO INTERACT WITH DATABASE
+// createBook()
+getBooks()
 
 server.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/index.html'))
