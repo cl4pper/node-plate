e571deab6527f4cf0b43a147cf001f911f56c210
cl4pper <cl4pper@gmail.com>
2019-12-26 20:44:36 -0300

    ADD: apply dotenv and import routes

--- server.js
+++ server.js
@@ -1,66 +1,22 @@
+require('dotenv').config()
+
 const path = require('path')
 const express = require('express')
 const server = express()
 const mongoose = require('mongoose')
 
+// LOCAL VARIABLES
+const PORT = 3000
+
 // MONGODB CONFIG. ---------------------------- START
-mongoose.connect('mongodb://localhost/library', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to mongodb...'))
     .catch(err => console.error('NOT connect to mongobd!', err))
-
-const bookSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    tags: [ String ],
-    date: { type: Date, default: Date.now },
-    isPublished: Boolean
-})
-
-const Book = mongoose.model('Book', bookSchema)
-
-// METHOD TO REGISTER DATA INTO DATABASE
-async function createBook () {
-    const book = new Book ({
-        title: 'Cronicas de Sharpe',
-        author: 'Bernard Cornwell',
-        tags: [ 'adventure', 'history' ],
-        isPublished: true
-    })
-
-    const result = await book.save()
-}
-
-// METHOD TO GET DATA FROM DATABASE
-async function getBooks () {
-    const result = await Book.find()
-        .limit(1)
-        .sort({ title: 1 })
-        // .count() -> to count items under filter properties
-        .select({ title: 1, tags: 1 })
-}
-
-// CALLING MTHODS TO INTERACT WITH DATABASE
-// createBook()
-getBooks()
-
 // MONGODB CONFIG. ---------------------------- END
 
-const PORT_MAIN = 3000
+server.use(express.json())
 
-server.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname+'/index.html'))
-})
+const libraryRoute = require('./routes/library')
+server.use('/', libraryRoute)
 
-server.listen(PORT_MAIN, () => {
-    console.log('Server is running on PORT:', PORT_MAIN)
-})
-
-// const http = require('http') - USING HTTP LIBRARY
-// let server = http.createServer((req, res) => {
-//     res.writeHead(200, { 'Content-type': 'text/plain'})
-//     res.end('Server running...')
-// })
-
-// server.listen(8080, () => {
-//     console.log('Server is running at 8080')
-// })
+server.listen(PORT, () => { console.log('Server is running on PORT:', PORT) })
